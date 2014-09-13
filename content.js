8a9421db5c3e9d0a6b44f1c8cc975ed4a948048c
Audasia Ho <audasiah@andrew.cmu.edu>
2014-09-13 15:44:13 -0400

    working bamboo words disappear

--- content.js
+++ content.js
@@ -1,18 +1,9 @@
 chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
   switch(message.type) {
     case "eat-bamboo":
-      //code to run when button is pressed
-      /*var divs = document.querySelectorAll("div");
-      if(divs.length === 0) {
-        alert("Therea re no divs");
-      }
-      else {
-        for(var i = 0; i < divs.length; i++) {
-          divs[i].style.backgroundColor = message.color;
-        }
-      }*/
-      document.body.innerHTML = document.body.innerHTML.replace(new RegExp("bamboo", "gi"), "poop");
-
+      $("*").each(function() {
+        $(this).html($(this).html().replace(/bamboo/gi, "<span style='opacity:0.0'>bamboo</span>"));
+      });
     break;
   }
 });
